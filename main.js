7bcab2cf2e43fc332bf03483d680120518c84961
0trebor0 <test@test.com>
2019-06-01 17:28:34 +0100

    commit

--- main.js
+++ main.js
@@ -12,13 +12,43 @@ try{
 	server.on( 'request', ( req, res )=>{
 		let parsed = url.parse( req.url, true );
 		console.log( req.socket.remoteAddress+" "+req.method+" "+req.url );
-		switch( parsed.pathname ){
-			case "/":
+		switch( req.method ){
+			case "GET":
+				//console.log( parsed );
+				if( parsed.pathname == '/video' ){
+					if( ytdl.validateID( parsed.query.id ) ){
+						//ytdl.getInfo( parsed.query.id, ( err, info )=>{} );
+						//res.writeHead( 206, {'Content-Type' : 'video/mp4'} );
+						//ytdl( "https://www.youtube.com/watch?v="+parsed.query.id ).pipe( res );
+						//ytdl( parsed.query.mp4 ).pipe( fs.createWriteStream('./video.mp4') );
+					} else if( ytdl.validateURL( parsed.query.url ) ){
+						//ytdl.getInfo( parsed.query.id, ( err, info )=>{} );
+						res.writeHead( 200, {'Content-Type' : 'video/mp4'} );
+						ytdl( parsed.query.url ).pipe( res );
+						//ytdl( parsed.query.mp4 ).pipe( fs.createWriteStream('./video.mp4') );
+					}
+				} else if( parsed.pathname == '/music' ){
+					if( ytdl.validateID( parsed.query.id ) ){
+						//res.writeHead( 206, {'Content-Type' : 'audio/mpeg'} );
+						//ytdl( "https://www.youtube.com/watch?v="+parsed.query.id, { filter: 'audioonly' } ).pipe( res );
+					} else if( ytdl.validateURL( parsed.query.url ) ){
+						res.writeHead( 200, {'Content-Type' : 'audio/mpeg'} );
+						ytdl( parsed.query.url, { filter: 'audioonly' } ).pipe( res );
+					}
+				} else {
+					res.writeHead( 404, {'Content-Type' : 'text/html'} );
+					res.write("hello");
+					res.end();
+				}
+			break;
+			case "POST":
+				req.on( 'data', ( chunk )=>{} );
+				req.on( 'end', ()=>{} );
+			break;
+			default:
 				res.writeHead( 404, {'Content-Type' : 'text/html'} );
 				res.write("hello");
 				res.end();
-			break;
-			default:
 			break;
 		}
 	} );
