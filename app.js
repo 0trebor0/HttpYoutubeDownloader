ada6bfadc770b9d32831eb8076f7af8839971fe6
0trebor0 <public0projects@outlook.com>
2019-09-14 15:10:11 +0100

    new changes

--- app.js
+++ app.js
@@ -12,46 +12,51 @@ module.exports = {
 		} );
 		this.server.on( 'request', ( req, res )=>{
 			let parsed = url.parse( req.url, true );
+			let postArray = {};
 			console.log( req.socket.remoteAddress+" "+req.method+" "+req.url );
 			if( req.method == 'GET' ){
-				switch( parsed.pathname ){
-					case "/":
-						if( ytdl.validateID( parsed.query.id ) ){
-							ytdl.getInfo( parsed.query.id , ( err, info )=>{
+				fs.createReadStream( __dirname+"/index.html" ).pipe( res );
+			} else if( req.method == 'POST' ){
+				req.on( 'data', ( chunk )=>{
+					postArray[ req.socket.remoteAddress ] = [];
+					postArray[ req.socket.remoteAddress ].push( chunk );
+				} );
+				req.on( 'end', ()=>{
+					postArray[ req.socket.remoteAddress ] = JSON.parse( postArray[ req.socket.remoteAddress ] );
+					console.log( postArray[ req.socket.remoteAddress ] );
+					if( postArray[ req.socket.remoteAddress ].url == null || postArray[ req.socket.remoteAddress ].url == '' ){
+						res.writeHead( 200, {'Content-Type' : 'application/json'} );
+						res.write( JSON.stringify({"type":"error","msg":"invalid Format"}) );
+						res.end();
+					} else {
+						if( ytdl.validateID( ytdl.getURLVideoID( postArray[ req.socket.remoteAddress ].url ) ) ){
+							ytdl.getInfo( ytdl.getURLVideoID( postArray[ req.socket.remoteAddress ].url ) , ( err, info )=>{
 								if( err ){
 									console.log( err );
+									res.writeHead( 200, {'Content-Type' : 'application/json'} );
+									res.write( JSON.stringify({"type":"error","msg":err}) );
+									res.end();
 								} else {
 									try{
 										let video = ytdl.chooseFormat(info.formats, { quality: 'highest' });
 										let audio = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
 										if( video ){
-											res.writeHead( 200, {'Content-Type' : 'text/html'} );
-											res.write("<style>body{text-align:center;}</style>");
-											res.write("<p><video src='"+video.url+"' controls></video></p>");
-											res.write("<p><audio src='"+audio.url+"' controls/></p>");
-											res.write("<p>Download video: <a href='"+video.url+"'>"+info.title+"</a></p>");
-											res.write("<p>Download audio: <a href='"+audio.url+"'>"+info.title+"</a></p>");
+											res.writeHead( 200, {'Content-Type' : 'application/json'} );
+											res.write( JSON.stringify( {"video":[{"url":video.url,"resolution":video.resolution,"encoding":video.encoding}],"audio":[{"url":audio.url,"audioEncoding":audio.audioEncoding,"audioBitrate":audio.audioBitrate}]} ) );
 											res.end();
+											delete postArray[ req.socket.remoteAddress ];
 										}
 									}catch(err){
 										console.log( err );
+										res.writeHead( 200, {'Content-Type' : 'application/json'} );
+										res.write( JSON.stringify({"type":"error","msg":err}) );
+										res.end();
 									}
 								}
 							} );
-						} else {
-							res.writeHead( 200, {'Content-Type' : 'text/html'} );
-							res.write("<style>body{text-align:center;}</style>");
-							res.write("<p><a href='/?id=PMGY8fLwess'>./?id=PMGY8fLwess</a></p>");
-							res.end();
 						}
-					break;
-					default:
-						res.writeHead( 200, {'Content-Type' : 'text/html'} );
-						res.write("<style>body{text-align:center;}</style>");
-						res.write("<p><a href='/?id=PMGY8fLwess'>./?id=PMGY8fLwess</a></p>");
-						res.end();
-					break;
-				}
+					}
+				} );
 			}
 		});
 	}
